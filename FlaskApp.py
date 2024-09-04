6af5ad74f000a2404d067dfbf2a7ccf9e6ddd662
I3WX <utkarshs7828@gmail.com>
2024-09-04 14:39:47 +0530

    now the result is in given detailed way and improved the root markdown

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 from conveter import *
 from flask_cors import CORS
+import markdown
 
 
 with open("README.md", "r", encoding="utf-8") as readme_file:
-    readme = readme_file.read()
+    readme = markdown.markdown(readme_file.read())
 
 app = Flask(__name__)
 
@@ -24,17 +25,47 @@ def convert():
     to_unit = request.args.get("to")
 
     if type == "length":
-        result = length_convert(value, from_unit, to_unit)
+        result = {
+            "converted_value": length_convert(value, from_unit, to_unit),
+            "value": value,
+            "from": from_unit,
+            "to": to_unit,
+        }
     elif type == "temp":
-        result = temp_convert(value, from_unit, to_unit)
+        result = {
+            "converted_value": temp_convert(value, from_unit, to_unit),
+            "value": value,
+            "from": from_unit,
+            "to": to_unit,
+        }
     elif type == "area":
-        result = convert_area(value, from_unit, to_unit)
+        result = {
+            "converted_value": convert_area(value, from_unit, to_unit),
+            "value": value,
+            "from": from_unit,
+            "to": to_unit,
+        }
     elif type == "speed":
-        result = convert_speed(value, from_unit, to_unit)
+        result = {
+            "converted_value": convert_speed(value, from_unit, to_unit),
+            "value": value,
+            "from": from_unit,
+            "to": to_unit,
+        }
     elif type == "mass":
-        result = convert_mass(value, from_unit, to_unit)
+        result = {
+            "converted_value": convert_mass(value, from_unit, to_unit),
+            "value": value,
+            "from": from_unit,
+            "to": to_unit,
+        }
     elif type == "data":
-        result = convert_data(value, from_unit, to_unit)
+        result = {
+            "converted_value": convert_data(value, from_unit, to_unit),
+            "value": value,
+            "from": from_unit,
+            "to": to_unit,
+        }
 
     if result is not None:
         return jsonify({"result": result})
